e7ba838b906cc5f9c606887f3e1a3b63ce24b1fc
Yashu26699 <yashaswipshah26699@gmail.com>
2020-06-12 15:17:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post("/",function (req,res) {
   };
 
   const jsonData = JSON.stringify(data);
-  const url = "https://us10.api.mailchimp.com/3.0/lists/xxxxx";
+  const url = "https://xxx.api.mailchimp.com/3.0/lists/xxxxx";
   const options = {
     method: "POST",
     auth: "xxxxx"
